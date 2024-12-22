73b28e35a115ab28ef98522cd86f6dc4cbe71824
Peter Dinis <peterdinis0@pobox.sk>
2024-12-22 20:50:15 +0100

    feat: Update Navigation design

--- app/components/shared/Navigation.tsx
+++ app/components/shared/Navigation.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { FC, useState } from "react";
 import {
@@ -29,23 +29,12 @@ export const AcmeLogo = () => {
 const Navigation: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
   return (
     <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
       </NavbarContent>
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
@@ -84,33 +73,41 @@ const Navigation: FC = () => {
 
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Button as={Link} color="primary" variant="flat" href="#">
+            Prihlásenie
+          </Button>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
+          <Button as={Link} color="secondary" href="#" variant="flat">
+            Registrácia
           </Button>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
+        <NavbarMenuItem>
+          <Link className="w-full" color="primary" href="#" size="lg">
+            Knihy
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="mt-2">
+          <Link className="w-full" color="primary" href="#" size="lg">
+            Kategórie
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="mt-2">
+          <Link className="w-full" color="primary" href="#" size="lg">
+            Spisovatelia
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem className="mt-2">
+          <Link className="w-full" color="primary" href="#" size="lg">
+            Vydavatelstvá
+          </Link>
+        </NavbarMenuItem>
       </NavbarMenu>
     </Navbar>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
