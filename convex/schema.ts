330481848f9a8abd22ead6a2994501cdc7689cd9
Peter Dinis <peterdinis0@pobox.sk>
2024-12-23 10:56:13 +0100

    feat: Create first tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,9 +2,23 @@ import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
 export default defineSchema({
-    books: defineTable({}),
+    books: defineTable({
+        id: v.string(),
+        name: v.string(),
+        description: v.string(),
+        image: v.string(),
+        year: v.number(),
+        pages: v.number(),
+        isAvaiable: v.boolean(),
+        authorName: v.string(),
+        publisherName: v.string()
+    }),
 
-    categories: defineTable({}),
+    categories: defineTable({
+        id: v.string(),
+        name: v.string(),
+        description: v.string()
+    }),
 
     authors: defineTable({}),
 
