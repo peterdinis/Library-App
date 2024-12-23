e672b6c3a19bbc2b0e7f7c45b569e845fea662f0
Peter Dinis <peterdinis0@pobox.sk>
2024-12-23 14:46:38 +0100

    feat: Writing query WIP

--- components/books/BookDetail.tsx
+++ components/books/BookDetail.tsx
@@ -1,6 +1,22 @@
+"use client"
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
 import { FC } from "react";
 
+
 const BookDetail: FC = () => {
+    const { id } = useParams();
+    const bookId = id![0] as unknown as Id<'books'>;
+
+    const data = useQuery(api.books.getBookById, {
+        id: bookId
+    })
+
+    console.log("D", data)
+
     return (
         <>
             prpr
