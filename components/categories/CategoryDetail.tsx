0d50d7243d11d9005fdd24c10c2988cad9062027
Peter Dinis <peterdinis0@pobox.sk>
2024-12-24 14:15:46 +0100

    feat: Add missing function for category detail

--- components/categories/CategoryDetail.tsx
+++ components/categories/CategoryDetail.tsx
@@ -1,6 +1,18 @@
-import { FC } from "react";
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Button, Chip, CircularProgress, Link } from "@nextui-org/react";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import {FC, Key, useMemo } from "react";
+import Empty from "../shared/Empty";
+import Header from "../shared/Header";
 
 const CategoryDetail: FC = () => {
+    const { id } = useParams();
+    const categoryID = id as unknown as string
+
+    const data = useQuery(api.categories.)
     return (
         <>
             category detail
