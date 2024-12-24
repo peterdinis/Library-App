9a75641beb3a2a674c083ea98a20db8eb5d044dd
Peter Dinis <peterdinis0@pobox.sk>
2024-12-24 12:39:22 +0100

    test: Updating vitest setup WIP

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -3,14 +3,13 @@ import { describe, it, expect } from "vitest";
 import Header from "../components/shared/Header";
 
 describe("Header Component", () => {
-    it("renders the correct text when provided", () => {
-        const testText = "Hello, Vitest!";
-        render(<Header text={testText} />);
-    
-        // Check if the text is rendered
-        const headerElement = screen.getByRole("heading");
-        expect(headerElement).toBeDefined()
-        expect(headerElement).toHaveProperty(testText)
-      });
+  it("renders the correct text when provided", () => {
+    const testText = "Hello, Vitest!";
+    render(<Header text={testText} />);
 
+    // Check if the text is rendered
+    const headerElement = screen.getByRole("heading");
+    expect(headerElement).toBeDefined(); // Check if the element exists
+    expect(headerElement).toHaveTextContent(testText); // Check if it contains the correct text
+  });
 });
