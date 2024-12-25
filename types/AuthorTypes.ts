19e8af075aed8db37963c55672aa9cc08d654d04
Peter Dinis <peterdinis0@pobox.sk>
2024-12-25 17:43:26 +0100

    feat: Writing first author functions

---
+++ types/AuthorTypes.ts
@@ -0,0 +1,10 @@
+export type AuthorType = {
+    name: string;
+    _id?: string
+    description: string;
+    image: string;
+    isActive: boolean;
+    litPeriod: string;
+    bornDate: string;
+    deathDate?: string;
+}
