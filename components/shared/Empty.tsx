a61f74d6e38b63f8558445e43eae15a02d751797
Peter Dinis <peterdinis0@pobox.sk>
2024-12-25 17:58:26 +0100

    Feat: Create basic authors page

--- components/shared/Empty.tsx
+++ components/shared/Empty.tsx
@@ -8,8 +8,8 @@ type EmptyProps = {
 const Empty: FC<EmptyProps> = ({ text }: EmptyProps) => {
 	return (
 		<div className="mt-6 flex justify-center align-top">
-			<Ghost className="animate-ease-linear animate-normal animate-bounce" />
-			<p className="font-bold">{text}</p>
+			<Ghost className="animate-ease-linear animate-normal animate-bounce w-10 h-10" />
+			<p className="ml-3 text-xl font-bold">{text}</p>
 		</div>
 	);
 };
