b19f53f371769b153bad4f9ad2a60c03834864ea
Peter Dinis <peterdinis0@pobox.sk>
2024-12-25 15:32:07 +0100

    test: Updating tests WIP

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -11,4 +11,13 @@ describe("Header Component", () => {
 		expect(headerElement).toBeDefined();
 		expect(headerElement).toHaveTextContent(testText);
 	});
+
+	it("renders the failed text when provided", () => {
+		const testText = "Hello, Vitest!";
+		render(<Header text={testText} />);
+
+		const headerElement = screen.getByRole("heading");
+		expect(headerElement).toBeDefined();
+		expect(headerElement).not.toHaveTextContent("Hello Vite")
+	});
 });
