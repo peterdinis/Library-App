270ed14997917bb2f8d23520abf8c5c7bb8ff7fc
Peter Dinis <peterdinis0@pobox.sk>
2024-12-26 10:28:36 +0100

    feat: Start working on authoe detail page WIP

---
+++ app/authors/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const AuthorDetailPage: NextPage = () => {
+    return (
+        <>
+            Author Detail
+        </>
+    )
+}
+
+export default AuthorDetailPage
