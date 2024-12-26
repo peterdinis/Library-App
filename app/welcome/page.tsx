cfad7582dab7011d45ec47be55f4d400c72ea616
peterdinis <petter1@pobox.sk>
2024-12-26 18:42:49 +0100

    infra: New welcome page

---
+++ app/welcome/page.tsx
@@ -0,0 +1,8 @@
+import WelcomeWrapper from "@/components/auth/WelcomeWrapper";
+import type { NextPage } from "next";
+
+const WelcomePage: NextPage = () => {
+    return <WelcomeWrapper />
+}
+
+export default WelcomePage
---
+++ components/auth/WelcomeWrapper.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const WelcomeWrapper: FC = () => {
+    return (
+        <>
+            Welcome
+        </>
+    )
+}
+
+export default WelcomeWrapper
