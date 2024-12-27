70a536a9658c3ca64a65f0ce7961332aeb3be292
peterdinis <petter1@pobox.sk>
2024-12-27 10:07:33 +0100

    infra: Start adding new page for create category

---
+++ app/categories/create/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const CreateCategoryPage: NextPage = () => {
+    return (
+        <>
+            ABC
+        </>
+    )
+}
+
+export default CreateCategoryPage
