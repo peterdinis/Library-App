a1c1e7ccc0dcad3e211f8bc8b00de1e0bdc9defb
peterdinis <petter1@pobox.sk>
2024-12-27 17:58:15 +0100

    feat: Start working on author create page

---
+++ app/authors/create/page.tsx
@@ -0,0 +1,8 @@
+import CreateAuthorForm from "@/components/authors/CreateAuthorForm";
+import { NextPage } from "next";
+
+const CreateNewAuthorPage: NextPage = () => {
+    return <CreateAuthorForm />
+}
+
+export default CreateNewAuthorPage;
---
+++ components/authors/CreateAuthorForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const CreateAuthorForm: FC = () => {
+    return (
+        <>
+            Create Author
+        </>
+    )
+}
+
+export default CreateAuthorForm
