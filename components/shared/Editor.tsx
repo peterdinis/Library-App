c6bbab92f45e8f88641cbcad1d574cf31b556e1c
peterdinis <petter1@pobox.sk>
2024-12-27 19:06:23 +0100

    feat: Adding new wsiying editor

--- components/shared/Editor.tsx
+++ components/shared/Editor.tsx
@@ -1,17 +1,17 @@
+"use client"
 
 import { Block } from "@blocknote/core";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
-import { FC, useState } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import { useTheme } from "@/hooks/useTheme";
 import "@blocknote/mantine/style.css";
 import "@blocknote/core/fonts/inter.css";
 
 const Editor: FC = () => {
-  const [,setBlocks] = useState<Block[]>([]);
+  const [, setBlocks] = useState<Block[]>([]);
   const { theme } = useTheme();
-
-  // Vytvorenie editora
+  
   const editor = useCreateBlockNote({
     initialContent: [
       {
@@ -20,15 +20,20 @@ const Editor: FC = () => {
       },
     ],
   });
-
-  // Dynamicky upravte pozadie na základe témy
-  const editorStyles = {
-    background: theme === "dark" ? "#121212" : "#ffffff",
-    color: theme === "dark" ? "#ffffff" : "#000000",
-  };
+  
+  const blockNoteRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (blockNoteRef.current) {
+      const editorElement = blockNoteRef.current.querySelector('.bn-editor') as HTMLElement;
+      if (editorElement) {
+        editorElement.style.backgroundColor = theme === "dark" ? "#121212" : "#ffffff";
+        editorElement.style.color = theme === "dark" ? "#ffffff" : "#000000";
+      }
+    }
+  }, [theme]);
 
   return (
-    <div style={editorStyles}>
+    <div ref={blockNoteRef}>
       <BlockNoteView
         onChange={() => setBlocks(editor.document)}
         editor={editor}
