dbd708d2c0b923d19ec94d18b67beb60a9a6dc97
peterdinis <petter1@pobox.sk>
2024-12-28 23:07:45 +0100

    FOO

--- app/welcome/page.tsx
+++
@@ -1,8 +0,0 @@
-import WelcomeWrapper from "@/components/auth/WelcomeWrapper";
-import type { NextPage } from "next";
-
-const WelcomePage: NextPage = () => {
-	return <WelcomeWrapper />;
-};
-
-export default WelcomePage;
