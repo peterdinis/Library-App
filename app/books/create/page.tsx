f18bcf6bc96860fe162fc7e7b8d1bb429278d023
peterdinis <petter1@pobox.sk>
2024-12-29 09:43:42 +0100

    feat: Working on create book page

---
+++ app/books/create/page.tsx
@@ -0,0 +1,8 @@
+import CreateBookForm from "@/components/books/CreateBookForm";
+import type { NextPage } from "next";
+
+const CreateBookPage: NextPage = () => {
+    return <CreateBookForm />
+}
+
+export default CreateBookPage
---
+++ components/books/CreateBookForm.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const CreateBookForm: FC = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default CreateBookForm
