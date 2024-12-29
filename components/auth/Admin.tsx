fd20033e69a506f5a8f9cbd8e34d1c83ceec9776
peterdinis <petter1@pobox.sk>
2024-12-29 18:15:48 +0100

    feat: Update our logic for admin auth component

--- components/auth/Admin.tsx
+++ components/auth/Admin.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
 import { type FC, type ReactNode, useMemo } from "react";
 import Empty from "../shared/Empty";
 
@@ -11,32 +10,21 @@ interface AdminProps {
 
 const Admin: FC<AdminProps> = ({ children }) => {
 	const { user, isLoaded } = useUser();
-	const router = useRouter();
 
-	// Check if the user has admin access
 	const hasAdminAccess = useMemo(() => {
 		return user?.organizationMemberships?.some(
 			(item) => item?.organization.name === "Admins",
 		);
 	}, [user]);
 
-	// If the user data is still loading or not available, show an empty state or a loader
 	if (!isLoaded) {
-		return <Empty text="Loading..." />;
+		return <Empty text="Presmeruvávam..." />;
 	}
 
-	// If no user is logged in, redirect to the login page (or another appropriate route)
-	if (!user) {
-		router.push("/login"); // Change to your login page route
-		return null;
-	}
-
-	// If the user doesn't have admin access, show the access denied message
 	if (!hasAdminAccess) {
 		return <Empty text="K tejto akcií nemáte prístup" />;
 	}
-
-	// Render children if the user is logged in and has admin access
+    
 	return <>{children}</>;
 };
 
