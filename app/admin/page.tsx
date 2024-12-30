1e2ee77282685c801a9068b3ecab9e4837034e88
peterdinis <petter1@pobox.sk>
2024-12-30 12:23:12 +0100

    FOO

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,8 @@
+import AdminWrapper from "@/components/admin/AdminWrapper";
 import { NextPage } from "next";
 
 const AdminPage: NextPage = () => {
-    return (
-        <>
-            ADMIN
-        </>
-    )
+    return <AdminWrapper />
 }
 
 export default AdminPage
---
+++ components/admin/AdminWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const AdminWrapper: FC = () => {
+    return (
+        <>
+            aaaa
+        </>
+    )
+}
+
+export default AdminWrapper
