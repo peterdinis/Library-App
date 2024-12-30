d6ac9d2da6675ddcee432a683755dd34356c2b2c
peterdinis <petter1@pobox.sk>
2024-12-30 13:33:58 +0100

    FOO

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import Empty from "@/components/shared/Empty";
+import Header from "@/components/shared/Header";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import { NextPage } from "next";
 
 const AdminBooksPage: NextPage = () => {
+    const data = useQuery(api.books.allSelectBooks);
+
+    if(!data) return <Empty text="Knihy sa nenašli" />
     return (
         <>
-        
+            <Header text="Všetky knihy" />
         </>
     )
 }
---
+++ components/admin/books/AdminBooks.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const AdminBooks: FC = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default AdminBooks
