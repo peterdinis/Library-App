fd762a0c405d45c4e0f6b356aa6c797c46aa266d
peterdinis <petter1@pobox.sk>
2024-12-30 13:44:42 +0100

    FOO

--- app/bookings/me/page.tsx
+++ app/bookings/me/page.tsx
@@ -1,11 +1,8 @@
+import MyBookings from "@/components/booking/MyBookings";
 import { NextPage } from "next";
 
 const MyBookingsPage: NextPage = () => {
-    return (
-        <>
-            MyBookings Page
-        </>
-    )
+    return <MyBookings />
 }
 
 export default MyBookingsPage
--- components/booking/MyBookings.tsx
+++ components/booking/MyBookings.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const MyBookings: FC = () => {
     return (
         <>
-        
+            Bookings
         </>
     )
 }
