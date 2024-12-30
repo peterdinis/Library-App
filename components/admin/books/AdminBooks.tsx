3e4902af127f76ca881558735af66bbc0f2c810b
peterdinis <petter1@pobox.sk>
2024-12-30 14:59:35 +0100

    infra: Working on admin pages WIP

--- components/admin/books/AdminBooks.tsx
+++ components/admin/books/AdminBooks.tsx
@@ -1,11 +1,34 @@
-import { FC } from "react";
+"use client";
+
+import type { FC } from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import GlobalErrorComponent from "@/components/shared/GlobalErrorComponent";
+import { DataTable } from "@/components/shared/GlobalTable";
+import Header from "@/components/shared/Header";
+import { Book } from "@/types/BookTypes";
+import { columns } from "./columns";
 
 const AdminBooks: FC = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+	const data = useQuery(api.books.allSelectBooks)
 
-export default AdminBooks
+	if (!data) {
+		return (
+			<GlobalErrorComponent
+				statusCode="404"
+				message="Knihy neboli nájdené"
+				linkHref="/admin/books"
+				linkText="Načítať znova"
+			/>
+		);
+	}
+
+	return (
+		<div className="mt-4">
+			<Header text="Zoznam všetkých kníh" />
+			<DataTable columns={columns} data={data as unknown as Book[]} />
+		</div>
+	);
+};
+
+export default AdminBooks;
