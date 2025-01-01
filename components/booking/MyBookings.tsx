72dcde8ca31279c84b6f478fb5e07d78203b8b9d
peterdinis <petter1@pobox.sk>
2025-01-01 16:47:32 +0100

    feat: Working on return book WIP

--- components/booking/MyBookings.tsx
+++ components/booking/MyBookings.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
 import type { FC } from "react";
 import MyBorrowedBooks from "./MyBorrowedBooks";
 import Settings from "./Settings";
 
 const MyBookings: FC = () => {
-	const { user } = useUser();
-
 	return (
 		<div className="grid md:grid-cols-2 md:gap-6 ml-4 mr-4">
 			<div className="space-y-6 mt-5">
--- components/booking/MyBorrowedBooks.tsx
+++ components/booking/MyBorrowedBooks.tsx
@@ -3,18 +3,31 @@
 import { useUser } from "@clerk/nextjs";
 import { Button, Card, CardBody, CardHeader, CircularProgress } from "@nextui-org/react";
 import type { FC, Key } from "react";
+import { useToast } from "@/hooks/useToast";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { BookingType } from "@/types/BookingTypes";
 
 const MyBorrowedBooks: FC = () => {
-	const { user } = useUser();
+	const { user } = useUser();	
+	const {toast} = useToast();
 
 	const data = useQuery(api.bookings.getBookingsByEmail, {
 		userEmail: user?.emailAddresses[0].emailAddress!
 	});
 
 	const returnBook = useMutation(api.bookings.returnBook);
+
+	const handleReturnBook = async(bookingId: string) => {
+		await returnBook({
+			bookingId
+		})
+		toast({
+			title: "Kniha bola vrátená",
+			duration: 2000,
+			className: "bg-green-800 text-white font-bold text-xl"
+		})
+	}
 
 	if(!data) return <CircularProgress />;
 
@@ -26,9 +39,9 @@ const MyBorrowedBooks: FC = () => {
 				</CardHeader>
 				<CardBody>
 					<div className="grid gap-4">
-						{data && data.map((item: BookingType, index: Key) => {
+						{data && data.map((item: BookingType) => {
 							return (
-								<div key={index} className="flex items-center space-x-4">
+								<div key={item._id} className="flex items-center space-x-4">
 									<div className="grid gap-1.5">
 										<h3 className="text-lg font-bold">{item.bookName}</h3>
 										<p className="text-sm font-medium leading-none">
@@ -38,7 +51,7 @@ const MyBorrowedBooks: FC = () => {
 											Do: {item.to}
 										</p>
 									</div>
-									<Button size="sm" className="ml-5" variant="solid">
+									<Button onPress={() => handleReturnBook(item._id)} size="sm" className="ml-5" variant="solid">
 										Vrátiť knihu
 									</Button>
 								</div>
