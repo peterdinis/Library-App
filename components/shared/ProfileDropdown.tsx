99d4965c26b0d7a820188936744c4d91afbff044
peterdinis <petter1@pobox.sk>
2025-01-02 13:23:54 +0100

    fix: Fixing clerk signout issue

--- components/shared/ProfileDropdown.tsx
+++ components/shared/ProfileDropdown.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useToast } from "@/hooks/useToast";
-import { useUser } from "@clerk/nextjs";
+import { useUser, useClerk } from "@clerk/nextjs";
 import {
 	Avatar,
 	Button,
@@ -14,38 +14,41 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import type { FC } from "react";
 
+
 const ProfileDropdown: FC = () => {
 	const { user } = useUser();
 	const router = useRouter();
 	const { toast } = useToast();
-
-	const logoutUser = () => {
-		toast({
-			title: "Odhlásenie bolo úspešné",
-			duration: 2000,
-			className: "bg-green-800 text-white font-bold text-xl",
-		});
-		router.push("/");
+	const {signOut} = useClerk();
+  
+	const logoutUser = async () => {
+	  await signOut();
+	  toast({
+		title: "Odhlásenie bolo úspešné",
+		duration: 2000,
+		className: "bg-green-800 text-white font-bold text-xl",
+	  });
+	  router.push("/"); // Redirect the user to the homepage
 	};
-
+  
 	return (
-		<Dropdown>
-			<DropdownTrigger>
-				<Avatar src={user?.imageUrl!} />
-			</DropdownTrigger>
-			<DropdownMenu>
-				<DropdownItem key="userName">{user?.fullName}</DropdownItem>
-				<DropdownItem key="myBorrowedBooks">
-					<Link href={"/bookings/me"}>Moje požičané knihy</Link>
-				</DropdownItem>
-				<DropdownItem key={"logout"}>
-					<Button size="sm" onPress={logoutUser} color="primary" variant="flat">
-						Odlhásenie
-					</Button>
-				</DropdownItem>
-			</DropdownMenu>
-		</Dropdown>
+	  <Dropdown>
+		<DropdownTrigger>
+		  <Avatar src={user?.imageUrl!} />
+		</DropdownTrigger>
+		<DropdownMenu>
+		  <DropdownItem key="userName">{user?.fullName}</DropdownItem>
+		  <DropdownItem key="myBorrowedBooks">
+			<Link href={"/bookings/me"}>Moje požičané knihy</Link>
+		  </DropdownItem>
+		  <DropdownItem key={"logout"}>
+			<Button size="sm" onPress={logoutUser} color="primary" variant="flat">
+			  Odhlásenie
+			</Button>
+		  </DropdownItem>
+		</DropdownMenu>
+	  </Dropdown>
 	);
-};
-
-export default ProfileDropdown;
+  };
+  
+  export default ProfileDropdown;
