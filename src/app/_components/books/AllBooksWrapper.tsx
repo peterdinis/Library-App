8d39670573adbff7cea08d8aa04d5e47fd28dd60
peterdinis <petter1@pobox.sk>
2025-01-26 17:45:55 +0100

    feat: New helper tailwind package

--- src/app/_components/books/AllBooksWrapper.tsx
+++ src/app/_components/books/AllBooksWrapper.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const AllBooksWrapper: FC = () => {
     return (
         <>
-            aaaaa
+            <h3 className="text-5xl prose prose-h3: font-bold text-zinc-800 dark:text-sky-50 text-center mt-4">Všetky knihy</h3>
         </>
     )
 }
