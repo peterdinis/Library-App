12bfd209e169d9c5860789abfc647637a8281dba
peterdinis <petter1@pobox.sk>
2025-01-26 17:49:12 +0100

    FOO

---
+++ src/app/(root)/books/[...id]/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import BookDetail from "~/app/_components/books/BookDetail";
+
+const BookDetailPage: NextPage = () => {
+    return <BookDetail />
+}
+
+export default BookDetailPage
