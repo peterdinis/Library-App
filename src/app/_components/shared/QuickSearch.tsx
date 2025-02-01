f900890e60c65d2301b16d4095ef61a0c4f39e0d
peterdinis <petter1@pobox.sk>
2025-02-01 19:49:05 +0100

    feat: Making quick search endpoint work WIP

--- src/app/_components/shared/QuickSearch.tsx
+++ src/app/_components/shared/QuickSearch.tsx
@@ -1,19 +1,27 @@
 "use client";
 
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogTitle,
-} from "~/components/ui/dialog";
-import { Search } from "lucide-react";
-import { FC, useState } from "react";
+import { Dialog, DialogTrigger, DialogContent, DialogTitle } from "~/components/ui/dialog";
+import { Loader2, Search, Frown, Ghost } from "lucide-react";
+import { FC, useState, ChangeEvent } from "react";
 import { Button } from "~/components/ui/button";
 import { DialogHeader } from "~/components/ui/dialog";
 import { Input } from "~/components/ui/input";
+import { api } from "~/trpc/react";
 
 const QuickSearch: FC = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const { data: books, isLoading, isError } = api.book.quickSearchBook.useQuery(searchQuery, {
+    enabled: searchQuery.length > 2,
+    retry: 1,
+    staleTime: Infinity
+  });
+
+  const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <Dialog open={isSearchOpen} onOpenChange={setIsSearchOpen}>
       <DialogTrigger asChild>
@@ -31,8 +39,26 @@ const QuickSearch: FC = () => {
           <DialogTitle>Rýchle vyhľadávanie</DialogTitle>
         </DialogHeader>
         <div className="flex gap-2 py-4">
-          <Input placeholder="Hľadaj kihu/y" className="flex-1" />
-          <Button>Hľadat</Button>
+          <Input
+            placeholder="Hľadaj knihu"
+            className="flex-1"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+          />
+          <Button>Hľadať</Button>
+        </div>
+        <div className="py-4">
+          {isLoading && <Loader2 className="animate-spin" />}
+          {isError && <p className="mt-4 font-bold text-2xl text-red-600"><Frown />Nastala chyba na strane applikácie</p>}
+          {books && books.length > 0 ? (
+            <ul>
+              {books.map((book) => (
+                <li key={book.id}>{book.title}</li>
+              ))}
+            </ul>
+          ) : (
+            searchQuery.length > 2 && <p className="font-bold prose prose-p: text-xl"><Ghost className="animate-bounce" /> Kniha sa nenašla</p>
+          )}
         </div>
       </DialogContent>
     </Dialog>
