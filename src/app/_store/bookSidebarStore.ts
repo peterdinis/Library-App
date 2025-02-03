5f26be33e08d64978f9a68aa9f8afe44ef76d03c
peterdinis <petter1@pobox.sk>
2025-02-03 18:40:44 +0100

    feat: Updating books page

--- src/app/_store/bookSidebarStore.ts
+++ src/app/_store/bookSidebarStore.ts
@@ -1,4 +1,4 @@
-import {create} from "zustand"
+import { create } from "zustand";
 
 type FilterState = {
   selectedCategory: string;
@@ -6,13 +6,18 @@ type FilterState = {
   selectedAuthor: string;
   setFilters: (category: string, genre: string, author: string) => void;
   clearFilters: () => void;
+  getFilters: () => { category: string; genre: string; author: string };
 };
 
-export const useFilterStore = create<FilterState>((set) => ({
+export const useFilterStore = create<FilterState>((set, get) => ({
   selectedCategory: "",
   selectedGenre: "",
   selectedAuthor: "",
   setFilters: (category, genre, author) =>
     set({ selectedCategory: category, selectedGenre: genre, selectedAuthor: author }),
   clearFilters: () => set({ selectedCategory: "", selectedGenre: "", selectedAuthor: "" }),
+  getFilters: () => {
+    const { selectedCategory, selectedGenre, selectedAuthor } = get();
+    return { category: selectedCategory, genre: selectedGenre, author: selectedAuthor };
+  },
 }));
