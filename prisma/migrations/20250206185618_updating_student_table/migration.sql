7520f6d5c6fd3e4f992a56f6af9eddad613223bd
peterdinis <petter1@pobox.sk>
2025-02-06 19:56:31 +0100

    infra: New Migration

---
+++ prisma/migrations/20250206185618_updating_student_table/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `universityCard` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `universityId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "class" TEXT NOT NULL DEFAULT '1.A',
+    "isTeacher" BOOLEAN NOT NULL DEFAULT false,
+    "lastActivityDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "email", "fullName", "id", "lastActivityDate", "password", "role", "status", "updatedAt") SELECT "createdAt", "email", "fullName", "id", "lastActivityDate", "password", "role", "status", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
