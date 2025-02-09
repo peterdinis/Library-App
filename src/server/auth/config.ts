e9e722fe0bbb096146475439a8dfc07dd0d4493a
peterdinis <petter1@pobox.sk>
2025-02-09 10:13:09 +0100

    feat: Email Next Auth Provider WIP

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,14 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { DefaultSession, NextAuthConfig } from "next-auth";
-
 import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
 	interface Session extends DefaultSession {
 		user: {
@@ -24,22 +17,8 @@ declare module "next-auth" {
 	// }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authConfig = {
 	providers: [
-		/**
-		 * ...add more providers here.
-		 *
-		 * Most other providers require a bit more work than the Discord provider. For example, the
-		 * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-		 * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-		 *
-		 * @see https://next-auth.js.org/providers/github
-		 */
 	],
 	session: {
 		strategy: "jwt",
