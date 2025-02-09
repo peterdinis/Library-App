f009d559ea1a662c803132b7d03be904fb49957c
peterdinis <petter1@pobox.sk>
2025-02-09 10:28:27 +0100

    feat: New profile page

---
+++ src/app/(auth)/profile/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import ProfileWrapper from "~/app/_components/auth/ProfileWrapper";
+
+const ProfilePage: NextPage = () => {
+    return <ProfileWrapper />
+}
+
+export default ProfilePage
