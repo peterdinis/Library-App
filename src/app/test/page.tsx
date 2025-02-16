dafc056696e17c253438ad2b6de994a6e85d0666
peterdinis <petter1@pobox.sk>
2025-02-16 12:41:01 +0100

    FOO

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import { NextPage } from "next";
+import {
+    usePlateEditor,
+    Plate,
+    PlateContent,
+} from '@udecode/plate/react';
 
 const TestPage: NextPage = () => {
+    const editor = usePlateEditor();
     return (
-        <>
-        
-        </>
+        <Plate editor={editor}>
+            <PlateContent placeholder="Type..." />
+        </Plate>
     )
 }
 
