d8ef0702deadb7e8cbe2e7e9083e1a60ddde0e91
peterdinis <petter1@pobox.sk>
2025-02-16 15:13:52 +0100

    feat: Basic setup for draft-js editor

---
+++ src/app/_components/shared/AppEditor.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { FC } from 'react';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+const Editor = dynamic(
+    () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+    { ssr: false }
+);
+
+const AppEditor: FC = () => {
+    return <Editor
+
+    />
+};
+
+export default AppEditor
