ddb828042d8952eab688464784607de809247b4b
peterdinis <petter1@pobox.sk>
2025-02-16 15:04:11 +0100

    FOO

---
+++ src/app/_components/admin/categories/CreateCategoryForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const CreateCategoryForm: FC = () => {
+    return (
+        <>
+            Create category form
+        </>
+    )
+}
+
+export default CreateCategoryForm
