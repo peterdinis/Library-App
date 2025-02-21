bf6e2e1c4cfdebcf2c4a0eae797c87c4930ef023
peterdinis <petter1@pobox.sk>
2025-02-21 20:58:39 +0100

    feat: Create category WIP

--- src/app/_components/admin/categories/CreateCategoryForm.tsx
+++ src/app/_components/admin/categories/CreateCategoryForm.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 
 const CreateCategoryForm: FC = () => {
-  return <>Create category form</>;
+  return <>
+    <h1 className="flex justify-center items-center mt-10 text-5xl font-bold">Vytvor novú kategóriu</h1>
+  </>;
 };
 
 export default CreateCategoryForm;
