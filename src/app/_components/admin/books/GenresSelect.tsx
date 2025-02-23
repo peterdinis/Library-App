92e493f04cac115acee5be56e35e9a873b46f128
peterdinis <petter1@pobox.sk>
2025-02-23 12:12:44 +0100

    feat: Create genre select component

--- src/app/_components/admin/books/GenresSelect.tsx
+++ src/app/_components/admin/books/GenresSelect.tsx
@@ -1,9 +1,39 @@
+"use client"
+
+import { Label } from "~/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "~/components/ui/select";
+import { Loader2 } from "lucide-react";
 import { FC } from "react";
+import { api } from "~/trpc/react";
 
 const GenresSelect: FC = () => {
+    const { data, isLoading } = api.genre.getAllGenres.useQuery()
+
+    if (isLoading) return <Loader2 className="animate-spin w-8 h-8" />
+
+
     return (
         <>
-        
+            <Label htmlFor="genre">Vybrať žáner</Label>
+            <Select
+            >
+                <SelectTrigger id="genre">
+                    <SelectValue placeholder="Vybrať žáner" />
+                </SelectTrigger>
+                <SelectContent>
+                    {data?.map((genre) => (
+                        <SelectItem key={genre.id} value={genre.id}>
+                            {genre.name}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
         </>
     )
 }
