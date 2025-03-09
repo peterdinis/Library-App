7ea3c9e41dd3664346b1795265db15085211a386
peterdinis <petter1@pobox.sk>
2025-03-09 11:19:46 +0100

    feat: Writing first booking endpoints

--- src/server/api/routers/booking.ts
+++ src/server/api/routers/booking.ts
@@ -1,3 +1,38 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { db } from "~/server/db";
+import z from "zod"
 
-export const bookingRouter = createTRPCRouter({});
+export const bookingRouter = createTRPCRouter({
+    getAllBookings: publicProcedure.query(async () => {
+        return await db.borrowRecord.findMany();
+    }),
+
+    getBookingDetail: publicProcedure
+        .input(z.string())
+        .query(async ({ input }) => {
+            return await db.borrowRecord.findUnique({
+                where: { id: input },
+            });
+        }),
+
+    getAllUsersBookings: publicProcedure
+        .input(
+            z.object({
+                userId: z.string(),
+            }),
+        ).query(async ({ input }) => {
+            const oneUser = await db.user.findFirst({
+                where: {
+                    id: input.userId
+                }
+            })
+
+            const allUsersBooking = await db.borrowRecord.findMany({
+                where: {
+                    userId: oneUser!.id
+                }
+            })
+
+            return allUsersBooking
+        })
+});
