1c854073a84131b620f32415e4c6055713ab84de
peterdinis <petter1@pobox.sk>
2025-03-09 11:29:56 +0100

    feat: Updating root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,11 +7,6 @@ import { categoryRouter } from "./routers/category";
 import { genreRouter } from "./routers/genre";
 import { userRouter } from "./routers/user";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
   book: bookRouter,
   category: categoryRouter,
@@ -22,14 +17,5 @@ export const appRouter = createTRPCRouter({
   admin: adminRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
-
-/**
- * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
- */
 export const createCaller = createCallerFactory(appRouter);
