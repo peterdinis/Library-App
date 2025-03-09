a49a0160b5c1c595e6ea252bba0ab48460c2b045
peterdinis <petter1@pobox.sk>
2025-03-09 17:55:04 +0100

    infra: New hook for useTeacher

---
+++ src/hooks/useTeacher.ts
@@ -0,0 +1,20 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { useMemo } from "react"
+import { Role } from "~/types/applicationTypes"
+
+const useTeacher = () => {
+    const { data: session, status } = useSession()
+
+    const teacherUser = useMemo(() => {
+        return session?.user?.role === Role.TEACHER
+    }, [session])
+
+    return {
+        teacherUser,
+        isLoading: status === "loading",
+    }
+}
+
+export default useTeacher
