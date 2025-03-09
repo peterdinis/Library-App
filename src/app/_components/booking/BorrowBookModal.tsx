3554f67561c393f2272e0aa0a28a33a05199950c
peterdinis <petter1@pobox.sk>
2025-03-09 12:25:30 +0100

    feat: Start working on BorrowBookModal logic WIP

--- src/app/_components/booking/BorrowBookModal.tsx
+++ src/app/_components/booking/BorrowBookModal.tsx
@@ -15,7 +15,13 @@ import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { useToast } from "~/hooks/use-toast";
 
-const BorrowBookModal: FC = () => {
+type BorrowBookModalProps = {
+  bookId: string
+}
+
+const BorrowBookModal: FC<BorrowBookModalProps> = ({
+  bookId
+}: BorrowBookModalProps) => {
   const { toast } = useToast();
 
   const borrowFnForNow = () => {
