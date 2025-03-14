b0507fff6038cf8baba141de57cb3c31e833f6a3
peterdinis <petter1@pobox.sk>
2025-03-14 20:03:21 +0100

    Remove cypress

--- cypress/e2e/about.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("About Page tests", () => {
-  it("should visit localhost:3000/about", () => {
-    cy.visit("http://localhost:3000/about");
-    cy.url().should("include", "localhost:3000/about");
-  });
-});
