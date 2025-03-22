d4a3b588a2335f0a7026dd603a63c51077ae2d35
peterdinis <petter1@pobox.sk>
2025-03-22 18:06:26 +0100

    fix: Updating route for displaying allBookings to return user info

--- src/app/_components/admin/bookings/AdminBooking.tsx
+++ src/app/_components/admin/bookings/AdminBooking.tsx
@@ -2,8 +2,15 @@
 
 import { FC } from "react";
 import AdminSharedComponent from "../shared/AdminSharedComponent";
+import { api } from "~/trpc/react";
+import { Loader2 } from "lucide-react";
 
 const AdminBooking: FC = () => {
+  const { data, isLoading } = api.booking.getAllBookings.useQuery();
+
+  if (isLoading) return <Loader2 className="h-8 w-8 animate-spin" />;
+
+  console.log("D", data);
   return (
     <AdminSharedComponent showStats={false}>
       <main className="flex-1 overflow-auto p-4 sm:p-6">
