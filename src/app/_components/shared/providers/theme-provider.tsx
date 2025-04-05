21e24ea98905fc88062c0b5fad24e77186f37e77
peterdinis <petter1@pobox.sk>
2025-04-05 18:24:58 +0200

    FOO

--- src/app/_components/shared/providers/theme-provider.tsx
+++ src/app/_components/shared/providers/theme-provider.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import * as React from "react";
+import { ComponentProps, useEffect, useState } from "react";
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  const [mounted, setMounted] = React.useState(false);
+}: ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setMounted(true);
   }, []);
 
