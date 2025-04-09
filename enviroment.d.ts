83aa5a6fb700cbd5467fd453ad466e67119270a8
peterdinis <petter1@pobox.sk>
2025-04-09 18:48:54 +0200

    infra: New helper .d.ts file

---
+++ enviroment.d.ts
@@ -0,0 +1,17 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            AUTH_SECRET: string;
+            DATABASE_URL: string;
+            UPLOADTHING_TOKEN: string;
+            REDIS_ENDPOINT: string;
+            REDIS_TOKEN: string 
+            QSTASH_URL: string;
+            QSTASH_TOKEN: string;
+            QSTASH_CURRENT_SIGNING_KEY: string;
+            QSTASH_NEXT_SIGNING_KEY: string;
+        }
+    }
+}
+
+export {};
