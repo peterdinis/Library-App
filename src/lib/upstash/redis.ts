03faf9b15bbc697020dbb4f4e9ceb15e93d2dbe9
peterdinis <petter1@pobox.sk>
2025-04-10 19:37:51 +0200

    fix: Update redis setup

--- src/lib/upstash/redis.ts
+++ src/lib/upstash/redis.ts
@@ -3,6 +3,7 @@ import { Redis } from "@upstash/redis";
 const redis = new Redis({
   url: process.env.REDIS_ENDPOINT as unknown as string,
   token: process.env.REDIS_TOKEN as unknown as string,
+  keepAlive: true
 });
 
 export default redis;
