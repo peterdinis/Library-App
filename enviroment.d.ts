912293d5b2b60c66276a753ec4c86ab7ab4f62cb
peterdinis <petter1@pobox.sk>
2025-04-24 11:54:25 +0200

    infra: Update enviroment.d.ts

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -2,13 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       AUTH_SECRET: string;
+      NEXTAUTH_URL: string;
       DATABASE_URL: string;
       UPLOADTHING_TOKEN: string;
       REDIS_ENDPOINT: string;
       REDIS_TOKEN: string;
       QSTASH_URL: string;
       QSTASH_TOKEN: string;
-      RESEND_TOKEN: string;
       QSTASH_CURRENT_SIGNING_KEY: string;
       QSTASH_NEXT_SIGNING_KEY: string;
       NODEMAILER_USER: string;
